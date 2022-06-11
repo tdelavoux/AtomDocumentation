ca72cf96c832f7cdf12a20139f398f631585a856
Thibaut <thibault.delavoux@gmail.com>
2022-06-11 14:45:41 +0200

    feature - Add Ligh notifications

--- resources/views/Documentation/content/components/alerts.blade.php
+++ resources/views/Documentation/content/components/alerts.blade.php
@@ -3,9 +3,10 @@
 @section('title_page', 'Atom Alerts')
 
 @section('content')
-    <section id="Components">
-        <h2>Alerts</h2>
 
+    <h2>Alerts</h2>
+    <section>
+    
         <h3>Basic Alerts</h3>
         <article>
             <div> 
@@ -28,6 +29,34 @@
                 <div class="a-alert a-alert-info" role="alert">This is a info alert—check it out! </div>
                 <div class="a-alert a-alert-light" role="alert">This is a light alert—check it out!</div>
                 <div class="a-alert a-alert-dark" role="alert">This is a dark alert—check it out!</div>
+            </pre>
+        </article>
+
+    </section>
+
+    <section>
+        <h3>Light Alerts</h3>
+        <article>
+            <div> 
+                <div class="a-alert a-light-primary" role="alert">This is a primary alert—check it out!</div>
+                <div class="a-alert a-light-secondary" role="alert">This is a tertiary alert—check it out! </div>
+                <div class="a-alert a-light-success" role="alert">This is a success alert—check it out!</div>
+                <div class="a-alert a-light-danger" role="alert">This is a danger alert—check it out! </div>
+                <div class="a-alert a-light-warning" role="alert">This is a warning alert—check it out!</div>
+                <div class="a-alert a-light-info" role="alert">This is a info alert—check it out! </div>
+                <div class="a-alert a-light-light" role="alert">This is a light alert—check it out!</div>
+                <div class="a-alert a-light-dark" role="alert">This is a dark alert—check it out!</div>
+            </div>
+
+            <pre class="brush: html">
+                <div class="a-alert a-light-primary" role="alert">This is a primary alert—check it out!</div>
+                <div class="a-alert a-light-secondary" role="alert">This is a tertiary alert—check it out! </div>
+                <div class="a-alert a-light-success" role="alert">This is a success alert—check it out!</div>
+                <div class="a-alert a-light-danger" role="alert">This is a danger alert—check it out! </div>
+                <div class="a-alert a-light-warning" role="alert">This is a warning alert—check it out!</div>
+                <div class="a-alert a-light-info" role="alert">This is a info alert—check it out! </div>
+                <div class="a-alert a-light-light" role="alert">This is a light alert—check it out!</div>
+                <div class="a-alert a-light-dark" role="alert">This is a dark alert—check it out!</div>
             </pre>
         </article>
 
